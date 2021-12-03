455af1d928023c128919c4b3a853d406d4cc93c1
mashuptwice <info@mashup-tech.de>
2021-12-03 19:59:57 +0100

    changed restart method

--- smartmeterblinker.ino
+++ smartmeterblinker.ino
@@ -2,30 +2,23 @@
 This is a rudimentary piece of code to read the "1000 blinks per KWh" LED on smart energy meters in europe.
 It simply counts a change of brightness on a light dependent resistor and does some simple math to calculate 
 the actual energy usage in watts.
-
 The basic code is simulated in tinkercad just to get the calculations right in the first place
-
 The meter blinks 1000 times for every KWh => 1000b/KWh
 1000b/KWh = 1000b/1000Wh = 1b/1Wh = 16,666b/KWm = 0,2777b/KWs
-
-
 TO-DO
 -build breadboard prototype to get real life measurements 
 -adapt code to ESP8266s ADC
 -add MQTT functionality
-
 TO-DO later stages
 -create light-tight housing with ring magnet for mounting onto the smart meter
 -
-
-
 */
 
 //uncomment to enable debug messages to serial console
 //#define DEBUG
 
 //uncomment to set measurement interval to one second instead of one minute
-#define TINKERCAD
+//#define TINKERCAD
 
 const int ldrPin = A0;
 long int ldrCount = 0;
@@ -44,14 +37,10 @@ void setup()
   #endif
 }
 
-//function resets device
-void(* resetFunc) (void) = 0;
-
-
 void loop()
 {
-	ldrStatus = analogRead(ldrPin);
-	if (ldrStatus != ldrStatusLast)
+  ldrStatus = analogRead(ldrPin);
+  if (ldrStatus != ldrStatusLast)
     {
       #ifdef DEBUG
       Serial.println("status changed");
@@ -72,7 +61,7 @@ void loop()
   //triggers every second
   if (millis() - lastMillis > 1000)
     {
-      #ifdef DEBUG	
+      #ifdef DEBUG  
       Serial.println("another second");
       #endif
       lastMillis = millis();
@@ -87,12 +76,12 @@ void loop()
       //reset device every 46 days to prevent millis() from overflowing a unsigned long (happens at 49 days)
       if ( lastMillis >= 4000000000 )
       {
-      resetFunc();
+      ESP.restart();
       }
     }
-  #else 	
+  #else   
   //triggers every minute
-  	if (millis() - lastMillis > 60000)
+    if (millis() - lastMillis > 60000)
     {
       #ifdef DEBUG
       Serial.println("another minute");
