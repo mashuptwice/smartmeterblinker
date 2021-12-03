956f7efc25e5ef486057fc94e323bffe12364f86
mashuptwice <info@mashup-tech.de>
2021-12-03 22:09:46 +0100

    bugfix
    measurement was off by one decimal

--- smartmeterblinker.ino
+++ smartmeterblinker.ino
@@ -4,7 +4,7 @@ It simply counts a change of brightness on a light dependent resistor and does s
 the actual energy usage in watts.
 The basic code is simulated in tinkercad just to get the calculations right in the first place
 The meter blinks 1000 times for every KWh => 1000b/KWh
-1000b/KWh = 1000b/1000Wh = 1b/1Wh = 16,666b/KWm = 0,2777b/KWs
+1000b/KWh = 1000b/1000Wh = 1b/1Wh = 16,666b/KWm = 0,2777b/KWs = 0,01666b/Wm
 TO-DO
 -add MQTT functionality
 TO-DO later stages
@@ -15,8 +15,7 @@ TO-DO later stages
 #include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
 #include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
-#include <ArduinoOTA.h>
-
+#include <ArduinoMqttClient.h>
 
 //uncomment to enable debug messages to serial console
 //#define DEBUG
@@ -28,17 +27,28 @@ int ldrStatus = 0;
 int ldrStatusLast = 0;
 float watt = 0;
 unsigned long lastMillis = 0;
+unsigned long lastPoll = 0;
 
-
+//mqtt configuration
+const char broker[] = "debian-vm";
+int        port     = 1883;
+const char topic[]  = "wattmeter";
 
 WiFiManager wm;
+WiFiClient wc;
+MqttClient mqttClient(wc);
 
-void setup() {
+
+void setup() 
+{
     WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP    
     // put your setup code here, to run once:
     Serial.begin(115200);
     pinMode(LED_BUILTIN, OUTPUT);
     pinMode(ldrPin, INPUT);
+    digitalWrite(LED_BUILTIN, HIGH);
+    
+    
     #ifdef DEBUG
     Serial.println("debug messages enabled");
     #endif
@@ -50,13 +60,25 @@ void setup() {
 
     //automatically connect using saved credentials if they exist
     //If connection fails it starts an access point with the specified name
-    if(wm.autoConnect("AutoConnectAP")){
+    if(wm.autoConnect("AutoConnectAP"))
+    {
         Serial.println("connected...yeey :)");
     }
-    else {
+    else 
+    {
         Serial.println("Configportal running");
     }
 
+    if (!mqttClient.connect(broker, port)) 
+    {
+    Serial.print("MQTT connection failed! Error code = ");
+    Serial.println(mqttClient.connectError());
+    //while (1);
+    }
+    else
+    {
+      Serial.println("mqtt connection successful!");  
+    }
 
   // Port defaults to 8266
   // ArduinoOTA.setPort(8266);
@@ -71,48 +93,17 @@ void setup() {
   // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
   // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
 
-  ArduinoOTA.onStart([]() {
-    String type;
-    if (ArduinoOTA.getCommand() == U_FLASH) {
-      type = "sketch";
-    } else { // U_FS
-      type = "filesystem";
-    }
-
-    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
-    Serial.println("Start updating " + type);
-  });
-  ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) {
-      Serial.println("Auth Failed");
-    } else if (error == OTA_BEGIN_ERROR) {
-      Serial.println("Begin Failed");
-    } else if (error == OTA_CONNECT_ERROR) {
-      Serial.println("Connect Failed");
-    } else if (error == OTA_RECEIVE_ERROR) {
-      Serial.println("Receive Failed");
-    } else if (error == OTA_END_ERROR) {
-      Serial.println("End Failed");
-    }
-  });
-  ArduinoOTA.begin();
   Serial.println("Ready");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
+  digitalWrite(LED_BUILTIN, LOW);
+     
 }
-void loop() {
-    wm.process();
-    // put your main code here, to run repeatedly:
-    ArduinoOTA.handle();
-   
-    
+void loop() 
+{
+     
+      wm.process();   
+      
     ldrStatus = analogRead(ldrPin);
   if (ldrStatus != ldrStatusLast)
     {
@@ -120,6 +111,7 @@ void loop() {
       Serial.println("status changed");
       Serial.println(ldrStatus);
       #endif
+      //hysteresis
       if ( (ldrStatus + 10) <= ldrStatusLast)
       {
         #ifdef DEBUG
@@ -129,7 +121,18 @@ void loop() {
       }
       ldrStatusLast = ldrStatus;
     }
-  
+  //poll to avoid being disconnected
+      if (millis() - lastPoll > 5000)
+      {
+      mqttClient.poll();
+      lastPoll = millis();
+      Serial.println("poll");
+      mqttClient.beginMessage("status");
+      mqttClient.print("poll");
+      mqttClient.endMessage();
+
+      
+      }
   //triggers every minute
     if (millis() - lastMillis > 60000)
     {
@@ -138,17 +141,32 @@ void loop() {
       #endif
       lastMillis = millis();
       Serial.println(ldrCount);
-      watt = ldrCount / 0.1666;
+      watt = ldrCount / 0.01666;
       Serial.println("");
       Serial.print(watt);
       Serial.print(" W");
       Serial.println("");
       ldrCount = 0;
 
+      mqttClient.beginMessage(topic);
+      mqttClient.print(watt);
+      mqttClient.endMessage();
+      
+      
+      
       //reset device every 46 days to prevent millis() from overflowing a unsigned long (happens at 49 days)
       if ( lastMillis >= 4000000000 )
       {
       ESP.restart();
       }
+    }
+     //something prevents MQTT messages being sent from within if loops, if this part is not in the main loop
+     mqttClient.beginMessage(topic);
+     mqttClient.endMessage();
+    
+     if (!mqttClient.connect(broker, port)) 
+    {
+      ESP.restart();
+    
     }
 }
